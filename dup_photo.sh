532025ad42bac667afdb53bd8bc6dff39382e142
Dang Nguyen <dang.nguyen@employmenthero.com>
2021-06-03 00:08:13 +0700

    update script

--- dup_photo.sh
+++ dup_photo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 albums_folder=test/albums
-collective_folder=test/collective
+collective_albums_folder=test/collective
 
 for album_folder in "$albums_folder"/*/ ; do
   echo "====== checking folder $album_folder ======"
@@ -9,15 +9,18 @@ for album_folder in "$albums_folder"/*/ ; do
   do
     album_file_name=`basename "${album_file}"`
     echo "*** Album File $album_file_name ***"
-    for collective_file in "$collective_folder"/*.*
-    do
-      collective_file_name=`basename "${collective_file}"`
-      echo "*** Collective File $collective_file_name ***"
-      if [[ "$album_file_name" == "$collective_file_name" ]]; then
-        echo "- file $collective_file_name match with $album_file_name."
-        rm "$collective_file"
-        echo "- Collective File $collective_file removed."
-      fi
+    for collective_album_folder in "$collective_albums_folder"/*/ ; do
+      for collective_file in "$collective_album_folder"/*.*
+      do
+        collective_file_name=`basename "${collective_file}"`
+        echo "*** Collective File $collective_file_name ***"
+        if [[ "$album_file_name" == "$collective_file_name" ]]; then
+          echo "- file $collective_file_name match with $album_file_name."
+          rm "$collective_file"
+          echo "- Collective File $collective_file removed."
+        fi
+      done
     done
+
   done
 done
