ffaa9ad86e9d3b4acdf34206534533e7c35805bd
Dang Nguyen <dang.nguyen@employmenthero.com>
2021-06-02 22:23:48 +0700

    init commit

---
+++ dup_photo.sh
@@ -0,0 +1,21 @@
+albums_folder=test/albums
+collective_folder=test/collective
+
+for album_folder in $albums_folder/*/ ; do
+  echo "====== checking folder $album_folder ======"
+  for album_file in $album_folder/*.*
+  do
+    album_file_name=`basename ${album_file}`
+    echo "*** Input File $album_file_name ***"
+    for collective_file in $collective_folder/*.*
+    do
+      collective_file_name=`basename ${collective_file}`
+      if [[ "$album_file_name" == "$collective_file_name" ]]; then
+        echo "- file $collective_file_name match with $album_file_name."
+        rm "$collective_file"
+        echo "- Collective File $collective_file removed."
+      fi
+    done
+  done
+
+done
