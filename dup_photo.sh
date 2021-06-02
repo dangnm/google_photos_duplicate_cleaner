0c30307e3be8b524b85200a4c66111d4d686ce31
Dang Nguyen <dang.nguyen@employmenthero.com>
2021-06-02 23:24:52 +0700

    update code

--- dup_photo.sh
+++ dup_photo.sh
@@ -1,15 +1,16 @@
 albums_folder=test/albums
 collective_folder=test/collective
 
-for album_folder in $albums_folder/*/ ; do
+for album_folder in "$albums_folder"/*/ ; do
   echo "====== checking folder $album_folder ======"
-  for album_file in $album_folder/*.*
+  for album_file in "$album_folder"/*.*
   do
-    album_file_name=`basename ${album_file}`
-    echo "*** Input File $album_file_name ***"
-    for collective_file in $collective_folder/*.*
+    album_file_name=`basename "${album_file}"`
+    echo "*** Album File $album_file_name ***"
+    for collective_file in "$collective_folder"/*.*
     do
-      collective_file_name=`basename ${collective_file}`
+      collective_file_name=`basename "${collective_file}"`
+      # echo "*** Collective File $collective_file_name ***"
       if [[ "$album_file_name" == "$collective_file_name" ]]; then
         echo "- file $collective_file_name match with $album_file_name."
         rm "$collective_file"
@@ -17,5 +18,4 @@ for album_folder in $albums_folder/*/ ; do
       fi
     done
   done
-
 done
